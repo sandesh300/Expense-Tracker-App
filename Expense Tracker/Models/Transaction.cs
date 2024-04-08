cf64fff9100b3a504fb71611c9ca91250ea08ab0
sandesh <bhujbalsandesh52@gmail.com>
2024-04-08 19:34:20 +0530

    Create Category and Transaction Models

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId{ get; set; } 
+        public Category Category { get; set; }
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
