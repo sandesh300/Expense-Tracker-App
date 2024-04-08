0fe94fc94c7c79c329baff49aaf610272a45479d
sandesh <bhujbalsandesh52@gmail.com>
2024-04-08 19:33:15 +0530

    Create Category and Transaction Models

---
+++ Expense Tracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Transaction> Transactions { get; set; }    
+        public DbSet<Category> Categories { get; set; }
+
+   
+               
+                );
+        }
+    }
+}
