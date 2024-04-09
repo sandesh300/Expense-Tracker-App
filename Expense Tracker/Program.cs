fd18c0e3003fdb327596e4058760f8fd15af5a75
sandesh <bhujbalsandesh52@gmail.com>
2024-04-09 18:05:11 +0530

    Add _SideBar Code

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
