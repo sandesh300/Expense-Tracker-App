22ff2ab36f5ac89ac9f8fb24267e342f53358018
sandesh <bhujbalsandesh52@gmail.com>
2024-04-09 17:25:49 +0530

    Add andUpdate code of transactionmodel

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -8,12 +8,35 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
 
-        public int CategoryId{ get; set; } 
-        public Category Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
         public int Amount { get; set; }
 
-        [Column(TypeName = "nvarchar(10)")]
+        [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
+
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
+
     }
 }
